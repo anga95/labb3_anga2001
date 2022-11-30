c6cf6757f9c2d765bba47d3bb1ff8e03b9873ffd
anga95 <andreas.gabrielsson@live.se>
2022-11-30 16:49:22 +0100

    class creation

--- main.cpp
+++ main.cpp
@@ -1,8 +1,19 @@
 #include <iostream>
+#include "p_queue.h"
 
+#define log(x) std::cout << x << std::endl;
 int main() {
+    p_queue<int> pq;
 
+    for (int i = 0; i < 10; ++i) {
+        pq.push(i);
+    }
+    std::cout << "size: "<< pq.size() << std::endl;
+    while (!pq.empty()){
+        std::cout << "#"<<pq.pop() << "\n";
+    }
 
+    std::cout << "size: "<< pq.size() << std::endl;
     std::cout << "Hello, World!" << std::endl;
     return 0;
 }
