f49bf938e99bd67f8030125955b3738c916e1687
anga95 <andreas.gabrielsson@live.se>
2022-12-01 23:18:35 +0100

    final

--- main.cpp
+++ main.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
 #include <utility>
 #include "p_queue.h"
+#include <ctime>
+
 
 //template<typename T>
 struct order{
@@ -15,18 +17,47 @@ struct std::less<order> {
     }
 };
 std::ostream& operator <<(std::ostream &os, const order& rhs){
-    os << rhs.name << "\t[" << rhs.value << "]\n";
+    os << rhs.name << "\t[" << rhs.value << "]";
     return os;
 }
-void placeOrders(std::string name, p_queue<order> &vec){
+void placeOrders(const std::string &name, p_queue<order> &vec){
     for (int i = 0; i < 7; i++) {
         order o = {name, ( 15+ rand() %  15)};
         vec.push(o);
     }
 }
+void broker(p_queue<order> &sellerList, p_queue<order> &buyerList){
+    order seller = sellerList.pop();
+    order buyer = buyerList.pop();
+    std::vector<std::pair<order, order>> sales;
+    while (!sellerList.empty() && !buyerList.empty()){
+
+        if (buyer.value >= seller.value){
+
+            std::cout<<"[" << buyer.value << "] >= [" << seller.value
+                     <<"] --- [" << buyer.name << "] köpte från [" << seller.name <<"]"<< std::endl;
+            sales.emplace_back(buyer,seller);
+            seller = sellerList.pop();
+        } else{
+            std::cout <<"Lägsta sälj pris: ["<<seller.value<< "]. Lägsta köp pris: ["<<buyer.value<<"] kr. "<<
+            buyer.name << " stryks från köp pga snåljåp" << std::endl;
+        }
+            buyer = buyerList.pop();
+
+    }
+    std::cout << std::endl;
+    std::cout << "Köp som genomfördes: [" << sales.size() <<"]" << "\n";
+    for (auto e: sales) {
+        std::cout << e.first << " köpte från " << e.second << "\n";
+    }
+}
+
+
 int main() {
+    std::srand(std::time(nullptr));
     p_queue<order> seller;
     p_queue<order> buyer;
+
     placeOrders("Erik Pendel", buyer);
     placeOrders("Jarl Wallenburg", buyer);
     placeOrders("Joakim con Anka", buyer);
@@ -35,14 +66,11 @@ int main() {
     placeOrders("Jarl Wallenburg", seller);
     placeOrders("Joakim con Anka", seller);
 
-    std::cout << "size: " << buyer.size() << std::endl << std::endl;
-    int count = 0;
-    while (!buyer.empty()){
-        std::cout << buyer.pop();
-    }
+    broker(seller,buyer);
+//    while (!buyer.empty()){
+//        std::cout << buyer.pop();
+//    }
 
-    std::cout << "size: " << buyer.size() << std::endl << std::endl;
-    std::cout << "Hello, World!" << std::endl;
     return 0;
 }
 //struct compare_by_price{
